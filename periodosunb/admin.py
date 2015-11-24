e748378af6150741c86a4be6b660e898d1270445
Marcus <marcusvinicius@cjr.rog.br>
2015-11-24 11:13:48 -0200

    templates/admin/base_site.html e primeiros passos para criar Views

--- periodosunb/admin.py
+++ periodosunb/admin.py
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 from .models import Periodo
 
-admin.site.register(Periodo)
+class PeriodoAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Período',			{'fields': ['ano','semestre']}),
+		('Admissões',		{'fields': ['admissao_vest','admissao_pas','admissao_transf_obrig','admissao_mudanca_curso','matricula_aluno_esp']}),
+		('Desligamentos',	{'fields': ['formados','transferencia_outras_ies','desligamento_voluntario','desligamento_rendimento','desligamento_abandono','desligamento_jubilamento','falecimento']}),
+		('Disciplinas',		{'fields': ['matriculas_disciplinas','aprovacao_disciplinas','reprovacao_disciplinas']}),
+		('Trancamentos',	{'fields': ['trancamento','tracamento_justificado','trancamento_geral_matr']}),
+		('Extras',			{'fields': ['cumpriram_monitoria']})
+	]
+
+admin.site.register(Periodo,PeriodoAdmin)
