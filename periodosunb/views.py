396db3bda4c116e0668ea2f5024e182a9b36eacf
Marcus <marcusvinicius@cjr.rog.br>
2015-11-30 15:59:19 -0200

    Deixando a view /periodosunb nos trinks

--- periodosunb/views.py
+++ periodosunb/views.py
@@ -7,16 +7,9 @@ from .models import Periodo
 
 
 def index(request):
-    #now = "15:50"
-    #html = "<html><title>Periodos UnB</title><body>It is now <strong>%s</strong>.</body></html>" % now
-    #template = loader.get_template('periodosunb/index.html')
-    
-
-    #string = ""
-    #for p in Periodo.objects.all():
-    #	string += "Periodo:%d/%d\n" % (p.ano, p.semestre)
-
-    return render(request, 'periodosunb/index.html')
+    periodos_list = Periodo.objects.order_by("ano")
+    context = {'periodos_list': periodos_list}
+    return render(request, 'periodosunb/index.html', context)
 
 def detail(request, periodo_id): # Teste
     return HttpResponse("You're looking at question %s." % periodo_id)
